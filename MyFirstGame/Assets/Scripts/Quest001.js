d5cc3ae8b5e6ed95f34f8cdab4235edc7470d193
kieranlewis <kieranlewis1334@gmail.com>
2017-07-30 19:49:01 +1200

    Tutorial 014
    Can now talk to the NPC to be given a quest

---
+++ MyFirstGame/Assets/Scripts/Quest001.js
@@ -0,0 +1,41 @@
+﻿import UnityEngine.UI;
+
+var TextBoxOnCheck : int = 0;
+var MessageBox : GameObject;
+var TextBox : GameObject;
+var TextMessage : String;
+var QuestBox : GameObject;
+var QuestText : GameObject;
+var QuestName : String;
+
+function OnMouseDown() {
+	if(TextBoxOnCheck == 0) {
+		TextBoxOnCheck = 1;
+		MessageBox.SetActive(true);
+		TextBox.GetComponent.<Text>().text = TextMessage;
+		QuestName = "Active Quest: Recover the Loot";
+		QuestText.GetComponent.<Text>().text = QuestName;
+	} else {
+		TextBoxOnCheck = 0;
+		MessageBox.SetActive(false);
+		TextMessage = "Villager: Please Help Traveller";
+	}
+}
+
+function Update() {
+	if(Input.GetButtonDown("Submit")) {
+		if(TextBoxOnCheck == 1) {
+			MessageBox.SetActive(false);
+			TextBoxOnCheck = 0;
+			TextMessage = "Villager: Please Help Traveller";
+		}
+	}
+
+	if(Input.GetButtonDown("Cancel")) {
+		if(TextBoxOnCheck == 1) {
+			MessageBox.SetActive(false);
+			TextBoxOnCheck = 0;
+			TextMessage = "Villager: Please Help Traveller";
+		}
+	}
+}
